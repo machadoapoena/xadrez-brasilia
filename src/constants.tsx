42586e7ff444fe6acd4e3b850ace2cc733e173d7
Apoena <machadoapoena@gmail.com>
2026-02-27 07:35:49 -0300

    feat(constants): Update partner data and types
    Changed the `id` type for partners from `string` to `number`.
    Updated the existing partner entries with new information and images, reflecting current partnerships.

--- src/constants.tsx
+++ src/constants.tsx
@@ -25,7 +25,7 @@ export const TODAY_MONTH = today.getMonth();
 export const TODAY_DAY = today.getDate();
 
 export interface Partner {
-  id: string;
+  id: number;
   name: string;
   role: string;
   description: string;
@@ -36,39 +36,57 @@ export interface Partner {
 
 export const PARTNERS: Partner[] = [
   {
-    id: '1',
-    name: 'Mequinho',
-    role: 'Grande Mestre',
-    description: 'Lenda do xadrez brasileiro, Mequinho é um dos maiores jogadores de todos os tempos. Oferece aulas e mentorias para todos os níveis.',
-    image: 'https://picsum.photos/seed/mequinho/200/200',
-    contact: '5561999999999',
-    instagram: 'https://www.instagram.com/mequinho_official/',
+    id: 1,
+    name: "Mearas",
+    role: "Escola de Xadrez",
+    contact: "5561981684530",
+    instagram: "https://www.instagram.com/mearassports/",
+    image: "https://fbx.org.br/wp-content/uploads/2019/12/Mearas-Escola-de-Xadrez-394x330.png",
+    description: "Referência no DF, especializada em festivais interescolares e formação de base sólida."
   },
   {
-    id: '2',
-    name: 'Rafael Leitão',
-    role: 'Grande Mestre',
-    description: 'Multicampeão brasileiro e renomado instrutor, Rafael Leitão compartilha sua vasta experiência em cursos e workshops.',
-    image: 'https://picsum.photos/seed/leitao/200/200',
-    contact: '5561988888888',
-    instagram: 'https://www.instagram.com/gmrafaelleitao/',
+    id: 2,
+    name: "Xadrez do Valle",
+    role: "Centro de Treinamento",
+    contact: "556183046464",
+    instagram: "https://www.instagram.com/xadrezvalle/",
+    image: "https://xadrezvalle.com.br/wp-content/uploads/2022/12/Marca-XadrezValle-2024-251x95.png",
+    description: "Cursos presenciais e online focados em performance competitiva e estratégia avançada."
   },
   {
-    id: '3',
-    name: 'Krikor Mekhitarian',
-    role: 'Grande Mestre',
-    description: 'Conhecido por seu estilo agressivo e didática envolvente, Krikor oferece treinamentos personalizados e análises de partidas.',
-    image: 'https://picsum.photos/seed/krikor/200/200',
-    contact: '5561977777777',
-    instagram: 'https://www.instagram.com/gm_krikor/',
+    id: 3,
+    name: "Mister Chess",
+    role: "Instrutor Online",
+    contact: "556182515940",
+    instagram: "https://www.instagram.com/nm.malcolm/",
+    image: "https://imagens.xadrezbrasilia.com/imagens/logo_misterchess.png",
+    description: "Treinamento online focados em performance competitiva e estratégia avançada."
   },
   {
-    id: '4',
-    name: 'Juliana Terao',
-    role: 'Mestra FIDE Feminina',
-    description: 'Referência no xadrez feminino, Juliana Terao inspira e capacita novas gerações de enxadristas com seu trabalho.',
-    image: 'https://picsum.photos/seed/juliana/200/200',
-    contact: '5561966666666',
-    instagram: 'https://www.instagram.com/julianaterao/',
+    id: 4,
+    name: "CID",
+    role: "Escola de Xadrez",
+    contact: "556181167386",
+    instagram: "https://www.instagram.com/cidxadreztag/",
+    image: "https://imagens.xadrezbrasilia.com/imagens/logo_cid.png",
+    description: "Aulas presenciais no CED 02 de Taguatinga- Centrão. As segundas, quartas e sextas. Gratuito. Horários 7h30, 8h30, 9h30 pela manhã, 14h30, 15h30 e 16h30 à tarde."
   },
+  {
+    id: 5,
+    name: "FM Luigy Lira",
+    role: "Instrutor",
+    contact: "5561998058689",
+    instagram: "https://www.instagram.com/liraluigy/",
+    image: "https://imagens.xadrezbrasilia.com/imagens/pf_luigy.png",
+    description: "Aulas personalizadas para todas as idades. Do iniciante ao avançado."
+  },
+  {
+    id: 6,
+    name: "Alexandre Bernardo",
+    role: "Instrutor",
+    contact: "5561982093023",
+    instagram: "https://www.youtube.com/@tioalexadrez",
+    image: "https://imagens.xadrezbrasilia.com/imagens/pf_alexbarb.png",
+    description: "Professor de Xadrez para crianças (básico ao avançado) e escritor de livros infantis para o ensino de Xadrez (A Jornada do Peão e Capivarada pela Editora Acolibri)."
+  }
 ];
